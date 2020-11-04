ae3eae8ec3ca7c939b3965fea60b08f24853f74a
Ignac <ignacio.hidalgo@hotmail.cl>
2020-11-03 23:43:59 -0300

    Prueba Evaluativa Modulo 1 Java

---
+++ Prueba1/src/modulo1/Asesoria.java
@@ -0,0 +1,20 @@
+package modulo1;
+
+/**
+ * Interface declara el método analizarUsuario.
+ * @author : Armando Cabrera, Ignacio Hidalgo, Daniel Duarte, Rodrigo Salas
+ * @version : 03/11/2020 
+ * @see <a href = "https://www.instagram.com/awakelab.cl" /> awakelab.com </a>
+ * 
+ * 
+ * */
+
+public interface Asesoria {
+
+	/**
+	 * Se declara método vacio.
+	 * @param sin papámetros.
+	 * */
+	public void analizarUsuario();
+
+}
